04086ac1f679b4883240e968bfcc1862a6d8a74d
mitruee <dimonshulman2@gmail.com>
2025-10-27 13:04:18 +0300

    ...

--- src/shulman_main.cpp
+++ src/shulman_main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <unordered_map>
 #include <regex>
 #include <limits>
 
@@ -92,7 +93,7 @@ int boolean() {
 class Metadata {
 private:
 
-	int id;
+	int id = 0;
 
 public:
 
@@ -103,7 +104,6 @@ public:
 class Pipeline {
 private:
 	
-	int id;
 	std::string name; // kilometer mark
 	float length;
 	int diameter;
@@ -111,36 +111,27 @@ private:
 
 public:
 
-	Pipeline(Metadata md, std::string n, float l, int d, bool s) {
-
-		id = md.getID();
-		name = n;
-		length = l;
-		diameter = d;
-		status = s;
-
-	}
+	Pipeline() : name(""), length(0.0f), diameter(0), status(false) {}
 
 	void setStatus(bool new_status) { status = new_status; }
 
-	const int &getID() const { return id; }
 	const std::string &getName() const { return name; }
-	const float &getLength() const { return length; }
-	const int &getDiameter() const { return diameter; }
-	const bool &getStatus() const { return status; }
+	const float getLength() const { return length; }
+	const int getDiameter() const { return diameter; }
+	const bool getStatus() const { return status; }
 
-	void pipelineConsoleInput() {
+	void consoleInput() {
 
 		cin_clear();
 		
-		std::cout << "=== COMPRESSOR CREATION MODE ===" << std::endl;
+		std::cout << "=== PIPELINE CREATION MODE ===" << std::endl;
 		std::cout << "Enter pipeline name: "; std::getline(std::cin>>std::ws, name);
 		std::cout << "Pipeline length. "; length = real();
 		std::cout << "Pipeline diameter. "; diameter = integer();
 		status = 0;
 	}
 
-	void pipelineFileInput(std::string filepath) {
+	void fileInput(std::string filepath) {
 		std::regex line_pattern("^\\s*(\\d+)\\s*;\\s*p\\s*;\\s*(.+)\\s*;\\s*(\\d+\\.\\d+)\\s*;\\s*(\\d+)\\s*;\\s*(0|1)\\s*$");
 		std::smatch match;
 		std::ifstream file(filepath);
@@ -149,7 +140,6 @@ public:
 		if (file.is_open()) {
 			while (std::getline(file, line)) {
 				if (std::regex_match(line, match, line_pattern)) {
-					id = std::stoi(match[1]);
 					name = match[2];
 					length = std::stof(match[3]);
 					diameter = std::stoi(match[4]);
@@ -163,7 +153,6 @@ public:
 class CompressorStation {
 private:
 
-	int id;
 	std::string name;
 	int workshops;
 	int involved_workshops;
@@ -171,29 +160,20 @@ private:
 
 public:
 
-	CompressorStation(Metadata md, std::string n, int w, int iw, char r) {
-
-		id = md.getID();
-		name = n;
-		workshops = w;
-		involved_workshops = iw;
-		rang = r;
-
-	}
+	CompressorStation() : name(""), workshops(0), involved_workshops(0), rang(0) {}
 
 	void setIW(int new_iw) { involved_workshops = new_iw; }
 
-	const int &getID() const { return id; }
 	const std::string &getName() const { return name; }
-	const float &getWorkshops() const { return workshops; }
-	const int &getIW() const { return involved_workshops; }
-	const bool &getRang() const { return rang; }
+	const int getWorkshops() const { return workshops; }
+	const int getIW() const { return involved_workshops; }
+	const bool getRang() const { return rang; }
 
-	void csConsoleInput() {
+	void consoleInput() {
 
 		cin_clear();
 
-		std::cout << "=== COMPRESSOR CREATION MODE ===" << std::endl;
+		std::cout << "=== COMPRESSOR STATION CREATION MODE ===" << std::endl;
 		std::cout << "Enter a station name: "; std::getline(std::cin, name);
 		std::cout << "Number of workshops. "; workshops = integer();
 		std::cout << "Number of involved workshops. ";
@@ -209,7 +189,7 @@ public:
 
 	}
 
-	void csFileInput(std::string filepath) {
+	void fileInput(std::string filepath) {
 
 		std::regex line_pattern("^\\s*(\\d+)\\s*;\\s*cs\\s*;\\s*(.+)\\s*;\\s*(\\d+)\\s*;\\s*(\\d+)\\s*;\\s*(A|B|C|D|E)\\s*$");
 		std::smatch match;
@@ -220,7 +200,6 @@ public:
 			while (std::getline(file, line)) {
 				if (std::regex_match(line, match, line_pattern)) {
 					if (0 < std::stoi(match[4]) < std::stoi(match[3])) {
-						id = std::stoi(match[1]);
 						name = match[2];
 						workshops = std::stoi(match[3]);
 						involved_workshops = std::stoi(match[4]);
@@ -247,8 +226,9 @@ void continuing() {
 }
 
 int main() {
-	Pipeline pipeline;
-	CompressorStation cs;
+	std::unordered_map<int, Pipeline> pls = {};
+	std::unordered_map<int, CompressorStation> css = {};
+	Metadata md;
 	std::string path = "test.txt";
 
 	while (true) {
@@ -285,25 +265,28 @@ int main() {
 		clear();
 
 		switch (command) {
-		case 0:
-			return 0;
-		case 1:
-
-			continuing(); continue;
-		case 2:
-			cs = csConsoleInput();
-
-			continuing(); continue;
-		case 3:
-		
-			continuing(); continue;
-		case 4:
-			continuing(); continue;
-		case 5:
+			case 0:
+				return 0;
+			case 1: {
+				Pipeline pl;
+				pl.consoleInput();
+				md.setID(md.getID() + 1);
+				auto [iter1, inserted1] = pls.insert_or_assign(md.getID(), pl);
 				continuing(); continue;
-		case 6:
-		case 7:
-			continuing(); continue;
+			}
+			case 2: {
+				CompressorStation cs;
+				cs.consoleInput();
+				md.setID(md.getID() + 1);
+				auto [iter2, inserted2] = css.insert_or_assign(md.getID(), cs);
+				continuing(); continue;
+			}
+			case 3: {
+				for (int id = 1; id <= md.getID(); id++) {
+					
+				}
+			}
 		}
 	}
+
 }
